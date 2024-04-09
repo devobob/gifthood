ccab86ac729c1b7a7a42408aa281871194bd0d1c
Vasilis Kefallinos <vkefallinos@gmail.com>
2024-04-09 19:10:43 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="max-h-fit">
-      <Image
-        className="launching"
-        src="/launchingsoon.png"
-        alt="Vercel Logo"
-        width={400}
-        height={24}
-        priority
-      />
+    <main className="max-h-fit py-10">
+      <div className="launching">
+        launching soon
+      </div>
+      <div className="catalog">
+        <p>until then...</p>
+        <p>discover our spring/summer stories</p>
+      </div>
       <Image
         className="banner"
         src="/aggelikh-flowers.png"
@@ -19,23 +18,37 @@ export default function Home() {
         height={2400}
         priority
       />
-      <a className="instagram" href="https://www.instagram.com/gifthood.gr/">
-        <Image
-          src="/insta.png"
-          alt="instagram"
-          width={50}
-          height={50}
-          priority
-        />
-      </a>
-      <Image
+      <div className="catalog-button">
+        <a href="/catalog.pdf" className="catalog-button-text">
+        <button className="catalog-button-text" >
+          HERE
+          </button>
+          </a>
+      </div>
+      <div className="instagram"
+      >
+        <a href="https://www.instagram.com/gifthood.curatedgifts/">
+          <Image
+            src="/insta.png"
+            alt="instagram"
+            width={50}
+            height={50}
+            priority
+          />
+        </a>
+
+      </div>
+      <div
         className="logo"
-        src="/logo.png"
-        alt="Vercel Logo"
-        width={300}
-        height={24}
-        priority
-      />
+      >
+        <div>
+          <p className="text-2xl font-bold text-center">GIFTHOOD</p>
+          <p className=" font-light text-center">CURATED GIFTS</p>
+
+        </div>
+ 
+        
+      </div>
     </main>
   );
 }
